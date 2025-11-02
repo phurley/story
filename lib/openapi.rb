0a4882057a9aa28d0ce26ee62036cb08cce301c5
Patrick Hurley <phurley@invoca.com>
2025-11-02 15:42:17 -0500

    handle runpod output too

--- lib/openapi.rb
+++ lib/openapi.rb
@@ -7,7 +7,7 @@ module OpenAPI
   class Client
     def initialize(model: 'qwen3-32b-awq', access_token: ENV['API_KEY'], uri_base: ENV['API_URL'])
       @model = model
-      @client = OpenAI::Client.new(access_token: access_token, uri_base: uri_base)
+      @client = OpenAI::Client.new(api_key: access_token, base_url: uri_base)
     end
 
     def chat(messages: [], options: {})
